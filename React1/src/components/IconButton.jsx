5d30b9dbe8c05818a90a28b672e9bbd33fd9941f
Michaela Estrera <michaela.destrera@gmail.com>
2025-09-04 10:16:56 +0800

    idea for IconButton - not implemented yet

---
+++ React1/src/components/IconButton.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import './Button.css'; 
+// function IconButton({ iconClass, children, onClick }) {
+//   return (
+//     <button className="btn" onClick={onClick}>
+//       <i className={`fa ${iconClass}`} aria-hidden="true"></i>
+//       {children && <span style={{ marginLeft: '0.5em' }}>{children}</span>}
+//     </button>
+//   );
+// }
+
+// export default IconButton;
+
+
+// ideally tiny icon then text 
