41740e67dfb183a38ffc9a3107df39522881343f
LAB63_USER ON WS07 <L11Y08W07@LabHCI.net>
2025-09-04 08:31:57 +0800

    Added top bar

---
+++ React1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "./components/TopBar.jsx";
+
+function App() {
+  return (
+    <div>
+      <TopBar />
+      {/* You can add more components here later */}
+    </div>
+  );
+}
+
+export default App;
