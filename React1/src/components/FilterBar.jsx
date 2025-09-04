04c4398c39e53826b6e810dc0d321440990d3db1
LAB63_USER ON WS07 <L11Y08W07@LabHCI.net>
2025-09-04 08:53:15 +0800

    Changed alignment of filter bar components to horizontal

--- React1/src/components/FilterBar.jsx
+++ React1/src/components/FilterBar.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import Subheading from "./Subheading.jsx";  
 import Dropdown from "./Dropdown.jsx";
-import "./FilterBar.css"; 
-import "./Dropdown.css";  
+import "./FilterBar.css";
 
 function FilterBar({ tags = [], types = [], onTagSelect, onTypeSelect }) {
   return (
     <div className="filter-bar">
-      <div className="filter-section">
-        <Subheading>Game Tags</Subheading>
-        <Dropdown label="Select Tag" options={tags} onSelect={onTagSelect} />
-      </div>
-      <div className="filter-section">
-        <Subheading>Game Types</Subheading>
-        <Dropdown label="Select Type" options={types} onSelect={onTypeSelect} />
-      </div>
+      <Subheading className="filter-label">Game Tags</Subheading>
+      <Dropdown label="Select Tag" options={tags} onSelect={onTagSelect} />
+      <Subheading className="filter-label">Game Types</Subheading>
+      <Dropdown label="Select Type" options={types} onSelect={onTypeSelect} />
     </div>
   );
 }
